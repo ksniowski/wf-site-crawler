312a76c3f5ea97f96e53b3de1d736287e29b80b3
Karol Śniowski <ksniowski@squiz.pl>
2025-02-13 12:23:23 +0100

    Cookie creation for different urls, urls-crawled.txt added, prevent from crashing when CTRL + C

--- crawler.js
+++ crawler.js
@@ -2,33 +2,59 @@ const { chromium } = require('playwright');
 const fs = require('fs');
 const { error } = require('console');
 
-// If needed - define the cookie
-const cookie = {
-    name: 'cookie name',
-    value: 'cookie value',
-    domain: 'your domain', //for example: www.google.com
-    path: '/',
-    httpOnly: true,
-    secure: false
-};
-
 (async () => {
     const startTime = Date.now();
-    const browser = await chromium.launch({ headless: false, slowMo: 500 });
+    const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext();
     const page = await context.newPage();
+
+    // Break loop after process termination
+    // Prevents going through elements while termination
+    process.on('SIGINT', function () {
+        console.warn(' Crawling stopped...');
+        process.exit();
+    });
 
     // Set global timeouts
     page.setDefaultTimeout(60000);
     page.setDefaultNavigationTimeout(60000);
 
-    // If needed - add the cookie to the browser context
-    // await context.addCookies([cookie]);
-
-    const urls = fs.readFileSync('urls.txt', 'utf-8')
+    // Get urls from the file
+    const urls = fs
+        .readFileSync('urls.txt', 'utf-8')
         .split('\n')
-        .map(url => url.trim())
-        .filter(url => url.length > 0);
+        .map((url) => url.trim())
+        .filter((url) => url.length > 0);
+
+    // Gather uniqe urls
+    let domains = {};
+    let uniqueUrls = urls.filter(function (url) {
+        let domain = new URL(url);
+        if (domains[domain.host]) {
+            // we have seen this domain before, so ignore the URL
+            return false;
+        }
+        // mark domain, retain URL
+        domains[domain.host] = true;
+        return true;
+    });
+
+    // Gather uniqe hosts for cookie Array creation
+    const uniqeHosts = uniqueUrls.map((el) => new URL(el).host);
+
+    // If needed - define the cookie which will be used for all the urls
+    // Cookie Array creation
+    const cookiesForAllUrls = uniqeHosts.map((el) => ({
+        name: 'SUP_COOKIE',
+        value: 'new',
+        domain: '.' + el,
+        path: '/',
+        httpOnly: true,
+        secure: false,
+    }));
+
+    // If needed - add the cookie Array to the browser context
+    await context.addCookies(cookiesForAllUrls);
 
     const failedUrls = [];
     let crawledCount = 0;
@@ -40,24 +66,44 @@ const cookie = {
         crawledCount++;
 
         try {
-            console.log(`Visiting: ${url}`);
+            let startTime = new Date();
+            const totalPercentage = (crawledCount * 100) / urls.length;
+            console.log(
+                `Visiting: ${crawledCount}/${
+                    urls.length
+                } [${totalPercentage.toFixed(2)}%]`
+            );
+            console.log(`URL: ${url}`);
 
             // Attempt to load the page
-            await page.goto(url, { waitUntil: 'load', timeout: 60000 });
-            await page.waitForLoadState('load');
+            await page.goto(url, { waitUntil: 'commit', timeout: 60000 });
 
-            console.log(`Fully Loaded: ${url}`);
+            let endTime = new Date();
+            let timeElapsed = (endTime - startTime) / 1000;
+            console.log(`✅ Load time: ${timeElapsed} seconds`);
+            console.log(`-----------------------------------------------`);
             successfulCount++;
 
-            //await page.waitForTimeout(500);
+            fs.appendFile('urls-crawled.txt', url + '\n', function (err) {
+                if (err) throw err;
+            });
 
+            const filePath = './urls.txt';
+
+            let urlsContent = fs.readFileSync(filePath).toString().split('\n'); // read file and convert to array by line break
+            urlsContent.shift(); // remove the the first element from array
+            urlsContent = urlsContent.join('\n'); // convert array back to string
+
+            fs.writeFileSync(filePath, urlsContent);
+
+            await page.waitForTimeout(500);
         } catch (error) {
             if (error.message.includes('TimeoutError')) {
-                console.log(`Timeout on: ${url}. Skipping...`);
-                timeoutCount++
+                console.log(`❌ Timeout on: ${url}. Skipping...`);
+                timeoutCount++;
             } else {
-                console.log(`Error on: ${url}. Skipping...`, error);
-                errorCount++
+                console.log(`❌ Error on: ${url}. Skipping...`, error);
+                errorCount++;
             }
 
             failedUrls.push(url);
@@ -79,7 +125,11 @@ const cookie = {
 
     const endTime = Date.now();
     const timeTaken = (endTime - startTime) / 1000;
-    console.log(`\nIt took ${timeTaken.toFixed(2)} seconds to crawl through ${crawledCount} pages.`);
+    console.log(
+        `\nIt took ${timeTaken.toFixed(
+            2
+        )} seconds to crawl through ${crawledCount} pages.`
+    );
 
     await browser.close();
 })();
